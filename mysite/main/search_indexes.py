64283e4210496fd825326c041a492e8bccbaad21
cdignam8304 <dignachr@gmail.com>
2020-05-11 23:19:54 +0200

    Minor updates

--- mysite/main/search_indexes.py
+++ mysite/main/search_indexes.py
@@ -1,7 +1,9 @@
 import datetime
 from haystack import indexes
-from .models import Hack
+from .models import Hack, HackSeries, HackCategory
 
+# Check out this page for querying the whoosh_index!
+# https://stackoverflow.com/questions/2395675/whoosh-index-viewer
 
 class HackIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
@@ -16,3 +18,31 @@ class HackIndex(indexes.SearchIndex, indexes.Indexable):
         """Used when the entire index for model is updated."""
         return self.get_model().objects.filter(hack_published__lte=datetime.datetime.now())
 
+
+class HackSeriesIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    hack_series = indexes.CharField(model_attr='hack_series')
+    series_summary = indexes.CharField(model_attr='series_summary')
+    # pub_date = indexes.DateTimeField(model_attr='pub_date')
+
+    def get_model(self):
+        return HackSeries
+
+    # N/A for HackSeries (?)
+    # def index_queryset(self, using=None):
+    #     """Used when the entire index for model is updated."""
+    #     return self.get_model().objects.filter(hack_published__lte=datetime.datetime.now())
+
+class HackCategoryIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    hack_category = indexes.CharField(model_attr='hack_category')
+    category_summary = indexes.CharField(model_attr='category_summary')
+    # pub_date = indexes.DateTimeField(model_attr='pub_date')
+
+    def get_model(self):
+        return HackCategory
+
+    # N/A for HackCategory (?)
+    # def index_queryset(self, using=None):
+    #     """Used when the entire index for model is updated."""
+    #     return self.get_model().objects.filter(hack_published__lte=datetime.datetime.now())
