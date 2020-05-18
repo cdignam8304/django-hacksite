b028e90154c1483cde857977ddfdeff303b10247
cdignam8304 <dignachr@gmail.com>
2020-05-18 18:42:11 +0200

    Search results template enhancements

--- mysite/main/forms.py
+++ mysite/main/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from haystack.forms import SearchForm
+from .models import Hack
 
 class NewUserForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -32,7 +33,8 @@ class AutocompleteSearchForm(SearchForm):
         
         # sqs = self.searchqueryset.filter(content_auto=self.cleaned_data["q"]) # works for hack content but no results from other fields
         # sqs = self.searchqueryset.filter(title_auto=self.cleaned_data["q"]) # works for hack title but no results from other fields
-        sqs = self.searchqueryset.filter(content=query) # Gets results from all indexed fields in all models BUT no partial text search. Full words, which behaves more like EdgeNgramField(?) NB: "content" is a special field in haystack to reference all fields/models in index.
+        # sqs = self.searchqueryset.filter(content=query) # Gets results from all indexed fields in all models BUT no partial text search. Full words, which behaves more like EdgeNgramField(?) NB: "content" is a special field in haystack to reference all fields/models in index.
+        sqs = self.searchqueryset.filter(content=query).models(Hack) # as above but only Hack models passed. So searching on a Series works but only the Hacks in the Series are returned, not the Series itself. Stemming is implemented by default so can search for "train" to return trains.
 
         if self.load_all:
             sqs = sqs.load_all()
